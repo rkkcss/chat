93cbfff281aacefae46c98fadc33105c6b5bb29f
rkkcss <galdani1997@gmail.com>
2023-01-16 09:23:09 +0100

    added websocket

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { LandingPage } from './pages/LandingPage/LandingPage';
-import { Login } from './pages/Login';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LandingPage } from "./pages/LandingPage/LandingPage";
+import { Login } from "./pages/Login";
 
 function App() {
   return (
-    <div style={{backgroundColor: 'yellow'}}>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Login />}></Route>
-          <Route path={'/home'} element={<LandingPage />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />}></Route>
+        <Route path={"/home"} element={<LandingPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
